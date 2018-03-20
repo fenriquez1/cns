9d201ea873a1eeecbd18b9c2dcd10d5de64c0069
Francisco Enriquez <Francisco.r.Enriquez@gmail.com>
2018-03-19 17:14:23 -0700

    Client and server files completed.

---
+++ client/client.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"crypto/sha1"
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"strconv"
+)
+
+const (
+	JoinReq     uint16 = 1
+	PassReq     uint16 = 2
+	PassResp    uint16 = 3
+	PassAccept  uint16 = 4
+	Data        uint16 = 5
+	Terminate   uint16 = 6
+	Reject      uint16 = 7
+	HdrSize     int    = 2
+	PyldLenSize int    = 4
+	PackIdSize  int    = 4
+)
+
+var (
+	nameNPort  []string
+	passwds    []string
+	outfile    string
+	joinReqArr = []byte{0x01, 0x00, 0x00, 0x00, 0x00, 0x00}
+)
+
+func usage() {
+	fmt.Printf("Usage: ./client <server name> <server port> <clientpwd1> <clientpwd2> <clientpwd3> <output file>\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func verifyDigest(pk []byte) {
+	pyldLen := binary.LittleEndian.Uint32(pk[0:])
+	recvDigest := pk[4:]
+	if int(pyldLen) != len(recvDigest) {
+		fmt.Println("ABORT")
+	}
+
+	data, err := ioutil.ReadFile(outfile)
+	checkError(err)
+
+	digest := sha1.Sum(data)
+	// fmt.Println(digest)
+	// fmt.Println(recvDigest)
+
+	if len(recvDigest) != len(digest) {
+		fmt.Println("ABORT")
+	}
+
+	for i := 0; i < len(digest); i++ {
+		if recvDigest[i] != digest[i] {
+			fmt.Println("ABORT")
+		}
+	}
+
+	fmt.Println("OK")
+}
+
+func handleConnection(conn net.Conn) {
+	// Send Join Request
+	conn.Write(joinReqArr)
+
+	buff := make([]byte, 1010)
+
+	passCount := 0
+
+	f, err := os.Create(outfile)
+	defer f.Close()
+	checkError(err)
+
+	// Read responses
+	for {
+		n, err := conn.Read(buff)
+		checkError(err)
+		header := binary.LittleEndian.Uint16(buff[0:])
+
+		switch header {
+		case PassReq:
+			passRespLen := HdrSize + PyldLenSize + len(passwds[passCount])
+			pyldLen := uint32(len(passwds[passCount]))
+			response := make([]byte, passRespLen)
+			binary.LittleEndian.PutUint16(response[0:], PassResp)
+			binary.LittleEndian.PutUint32(response[2:], pyldLen)
+			copy(response[6:], []byte(passwds[passCount]))
+			_, err := conn.Write(response)
+			checkError(err)
+			passCount++
+		case PassAccept:
+			//TODO Not sure if there is an action to take here
+		case Data:
+			// pkID := binary.LittleEndian.Uint32(buff[6:10])
+			data := buff[10:n]
+			_, err := f.Write(data)
+			checkError(err)
+			f.Sync()
+		case Reject:
+			fmt.Println("ABORT")
+			return
+		case Terminate:
+			verifyDigest(buff[2:n])
+			return
+		default:
+			fmt.Println("ABORT")
+			return
+		}
+	}
+}
+
+func main() {
+	args := os.Args
+	if len(args) != 7 {
+		usage()
+		return
+	}
+
+	// Parse command line args
+	nameNPort = args[1:3]
+	passwds = args[3:6]
+	outfile = args[6]
+
+	_, err := strconv.Atoi(nameNPort[1])
+	if err != nil {
+		usage()
+		return
+	}
+
+	// Connect to server
+	hostPort := nameNPort[0] + ":" + nameNPort[1]
+	conn, err := net.Dial("udp4", hostPort)
+	defer conn.Close()
+	checkError(err)
+
+	handleConnection(conn)
+}
---
+++ server/server.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"crypto/sha1"
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	JoinReq     uint16 = 1
+	PassReq     uint16 = 2
+	PassResp    uint16 = 3
+	PassAaccept uint16 = 4
+	Data        uint16 = 5
+	Terminate   uint16 = 6
+	Reject      uint16 = 7
+	HdrSize     int    = 2
+	PyldLenSize int    = 4
+	PackIdSize  int    = 4
+)
+
+var (
+	clientPassword string
+	inputFilePath  string
+	passReqArr     = []byte{0x02, 0x00, 0x00, 0x00, 0x00, 0x00}
+	passAccArr     = []byte{0x04, 0x00, 0x00, 0x00, 0x00, 0x00}
+	rejectArr      = []byte{0x07, 0x00, 0x00, 0x00, 0x00, 0x00}
+)
+
+func usage() {
+	fmt.Printf("Usage: ./server <port> <password> <input file>\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func terminate(pc net.PacketConn, addr net.Addr) {
+	// Get digest and send terminate packet
+	data, err := ioutil.ReadFile(inputFilePath)
+	checkError(err)
+	digest := sha1.Sum(data)
+	fmt.Println(digest)
+
+	packLen := HdrSize + PyldLenSize + sha1.Size
+	pack := make([]byte, packLen)
+	binary.LittleEndian.PutUint16(pack[0:], Terminate)
+	binary.LittleEndian.PutUint32(pack[2:], uint32(len(digest)))
+	copy(pack[6:], digest[0:])
+	pc.WriteTo(pack, addr)
+	fmt.Println("OK")
+}
+
+func sendFile(pc net.PacketConn, addr net.Addr) {
+	// Read file and send
+	f, err := os.Open(inputFilePath)
+	defer f.Close()
+	checkError(err)
+
+	fi, err := f.Stat()
+	checkError(err)
+
+	dat := make([]byte, 1000)
+	s := fi.Size()
+	packID := uint32(0)
+	for i := int64(0); i < s; {
+		n, err := f.Read(dat)
+		checkError(err)
+		packLen := HdrSize + PyldLenSize + PackIdSize + n
+		pack := make([]byte, packLen)
+		binary.LittleEndian.PutUint16(pack[0:], Data)
+		binary.LittleEndian.PutUint32(pack[2:], uint32(n))
+		binary.LittleEndian.PutUint32(pack[6:], packID)
+		copy(pack[10:], dat[0:n])
+		pc.WriteTo(pack, addr)
+		i += int64(n)
+		packID++
+	}
+}
+
+func handlePacketConnection(pc net.PacketConn) {
+	reqCount := 0
+	buff := make([]byte, 1010)
+	for {
+		count, clientAddr, err := pc.ReadFrom(buff)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		recvd := buff[0:count]
+		header := binary.LittleEndian.Uint16(buff[0:])
+
+		switch header {
+		case JoinReq:
+			pc.WriteTo(passReqArr, clientAddr)
+			reqCount++
+		case PassResp:
+			tPass := string(recvd[6:])
+			if strings.Compare(tPass, clientPassword) == 0 {
+				pc.WriteTo(passAccArr, clientAddr)
+				// Password accepted, now send file
+				sendFile(pc, clientAddr)
+				// Terminate
+				terminate(pc, clientAddr)
+				return
+			}
+
+			if reqCount < 3 {
+				pc.WriteTo(passReqArr, clientAddr)
+				reqCount++
+			} else {
+				pc.WriteTo(rejectArr, clientAddr)
+			}
+		default:
+			fmt.Println("ABORT")
+			return
+		}
+
+		fmt.Printf("Client Address = %s\n", clientAddr)
+		fmt.Printf("Buffer = %#4x\n", recvd)
+	}
+}
+
+func main() {
+	args := os.Args
+	if len(args) != 4 {
+		usage()
+		return
+	}
+
+	// Parse command line args
+	mPort := args[1]
+	clientPassword = args[2]
+	inputFilePath = args[3]
+
+	_, err := strconv.Atoi(mPort)
+	if err != nil {
+		usage()
+		return
+	}
+
+	addr := ":" + mPort
+	pc, err := net.ListenPacket("udp4", addr)
+	defer pc.Close()
+	checkError(err)
+
+	handlePacketConnection(pc)
+}
